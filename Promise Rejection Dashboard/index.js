194800f7e57e20872f50d8d4c29b10523c2a6afe
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-04-12 13:43:23 +0200

    Added the weather info

--- Promise Rejection Dashboard/index.js
+++ Promise Rejection Dashboard/index.js
@@ -27,3 +27,27 @@ try {
 } catch (err) {
     console.error(err);
 }
+
+function getCurrentTime() {
+    const date = new Date();
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", { timeStyle: "medium" });
+}
+setInterval(getCurrentTime, 1000);
+
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+      const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`);
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      const data = await res.json();
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} />
+        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+        <p class="weather-city">${data.name}</p>
+      `;
+    } catch (err) {
+      console.error(err);
+    }
+});
